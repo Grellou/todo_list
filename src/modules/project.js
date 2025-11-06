05b61cc428af4e5b644ffa5c6edda6b5c54b32ca
Grellou <aleknaharoldas@yahoo.com>
2025-11-06 21:11:37 +0200

    projects implementation

---
+++ src/modules/project.js
@@ -0,0 +1,19 @@
+export class Project {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.todos = [];
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  removeTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  getTodos() {
+    return this.todos;
+  }
+}
--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,10 +1,13 @@
 import { Todo } from "./todo.js";
+import { Project } from "./project.js";
 
+// Save todos
 export function saveTodos(todosArray) {
   const todosConvertedToJson = JSON.stringify(todosArray);
   localStorage.setItem("todos", todosConvertedToJson);
 }
 
+// Load todos from storage
 export function loadTodos() {
   const todosFromStorage = localStorage.getItem("todos");
   if (todosFromStorage) {
@@ -27,5 +30,48 @@ export function loadTodos() {
     }
   } else {
     return [];
+  }
+}
+
+// Save projects
+export function saveProjects(projectsArray) {
+  const projectsConvertedToJson = JSON.stringify(projectsArray);
+  localStorage.setItem("projects", projectsConvertedToJson);
+}
+
+// Load projects from storage
+export function loadProjects() {
+  const projectsFromStorage = localStorage.getItem("projects");
+
+  // Return default project if nothing is found in storage
+  if (!projectsFromStorage) {
+    const defaultProject = new Project(1, "default");
+    return [defaultProject];
+  } else {
+    try {
+      // Reconstruct projects
+      const plainProjects = JSON.parse(projectsFromStorage);
+      const projectInstances = plainProjects.map((obj) => {
+        const project = new Project(obj.id, obj.name);
+
+        // Reconstruct todos for this project
+        obj.todos.forEach((todoObj) => {
+          const todo = new Todo(
+            todoObj.title,
+            todoObj.description,
+            todoObj.dueDate,
+            todoObj.priority,
+          );
+          todo.completed = todoObj.completed;
+          project.addTodo(todo);
+        });
+        return project;
+      });
+      return projectInstances;
+    } catch (error) {
+      console.log("Error loading projects");
+      const defaultProject = Project(1, "default");
+      return [defaultProject];
+    }
   }
 }
